980ba7f0054863678ce26d72fe5b6a61fafd7d29
subhajeetch998989 <Subhajeetchowdhury954@gmail.com>
2024-10-31 02:29:21 +0530

    update

---
+++ app.js
@@ -0,0 +1,41 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+const fs = require('fs');
+const TOKEN = process.env.TOKEN;
+const serverID = process.env.SERVER_ID;
+
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Array of image file paths for icons
+const icons = ['images/animekun_blue.gif', 'images/dewali_animekun.gif', 'images/snow_animekun.gif']; // Add paths to your images here
+let currentIndex = 0;
+
+// Function to change server icon
+async function changeIcon() {
+    const guild = client.guilds.cache.get(serverID);
+    if (!guild) {
+      return console.log('Server not found.');
+      
+    } else {
+      console.log('server found')
+    }
+
+    try {
+        console.log('changing the icon!')
+        await guild.setIcon(fs.readFileSync(icons[currentIndex]));
+        console.log(`Changed icon to ${icons[currentIndex]}`);
+        currentIndex = (currentIndex + 1) % icons.length; // Cycle through images
+    } catch (error) {
+        console.error('Failed to change icon:', error);
+    }
+}
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    setInterval(changeIcon, 1 * 60 * 1000); // 1 minutes in milliseconds
+});
+
+// Log in to Discord
+client.login(TOKEN);
