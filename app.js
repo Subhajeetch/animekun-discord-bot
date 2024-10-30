3949831262cc85c1f43de019e028fdb110d21df7
subhajeetch998989 <Subhajeetchowdhury954@gmail.com>
2024-10-31 02:46:05 +0530

    update app.js

--- app.js
+++ app.js
@@ -4,24 +4,36 @@ const fs = require('fs');
 const TOKEN = process.env.TOKEN;
 const serverID = process.env.SERVER_ID;
 
-
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+
 // Array of image file paths for icons
 const icons = ['images/animekun_blue.gif', 'images/dewali_animekun.gif', 'images/snow_animekun.gif'];
+
+
+
+
+
+
 let currentIndex = 0;
 
 // Function to change server icon
 async function changeIcon() {
     const guild = client.guilds.cache.get(serverID);
     if (!guild) return console.log('Server not found.');
+    
+    const logChannel = guild.channels.cache.get('1262339592393330708');
+    if (!logChannel) return console.log('Log channel not found.');
 
     try {
         
         await guild.setIcon(fs.readFileSync(icons[currentIndex]));
-        console.log(`Changed icon to ${icons[currentIndex]}`);
-        currentIndex = (currentIndex + 1) % icons.length; // Cycle through images
+        
+        const message = `Changed icon to ${icons[currentIndex]}`;
+        await logChannel.send(message);
+        
+        currentIndex = (currentIndex + 1) % icons.length;
     } catch (error) {
         console.error('Failed to change icon:', error);
     }
@@ -29,7 +41,7 @@ async function changeIcon() {
 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    setInterval(changeIcon, 1 * 60 * 1000); // 1 minutes in milliseconds
+    setInterval(changeIcon, 1 * 60 * 1000);
 });
 
 // Log in to Discord
