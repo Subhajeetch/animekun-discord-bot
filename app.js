488dd65797bb2ad8114bd32143e152595b53f3ab
subhajeetch <subhajeetchowdhury954@gmail.com>
2024-11-08 23:24:04 +0530

    update app.js

--- app.js
+++ app.js
@@ -53,10 +53,43 @@ const avatars = [
 
 
 const statuses = [
-    { name: 'You Pooping', type: ActivityType.Watching },
-    { name: 'your Nonsense', type: ActivityType.Listening },
-    { name: 'with your Heart', type: ActivityType.Playing },
-    { name: 'Animekun.lol', type: ActivityType.Watching },
+  { name: 'You Pooping', type: ActivityType.Watching },
+  { name: 'your Nonsense', type: ActivityType.Listening },
+  { name: 'with your Heart', type: ActivityType.Playing },
+  { name: 'Animekun.lol', type: ActivityType.Watching },
+  { name: 'your excuses', type: ActivityType.Listening },
+  { name: 'you pretend to work', type: ActivityType.Watching },
+  { name: 'nice... for now!', type: ActivityType.Playing },
+  { name: 'you struggle', type: ActivityType.Watching },
+  { name: 'dumb', type: ActivityType.Playing },
+  { name: 'you panic', type: ActivityType.Watching },
+  { name: 'it cool', type: ActivityType.Playing },
+  { name: 'your mess', type: ActivityType.Watching },
+  { name: 'along', type: ActivityType.Playing },
+  { name: 'your every move', type: ActivityType.Watching },
+  { name: 'hide and seek', type: ActivityType.Playing },
+  { name: 'the world burn', type: ActivityType.Watching },
+  { name: 'your problems', type: ActivityType.Listening },
+  { name: 'your every mistake', type: ActivityType.Watching },
+  { name: 'with your mind', type: ActivityType.Playing },
+  { name: 'your problems', type: ActivityType.Listening },
+  { name: 'nice with everyone', type: ActivityType.Playing },
+  { name: 'you fail again', type: ActivityType.Watching },
+  { name: 'endless drama', type: ActivityType.Listening },
+  { name: 'with broken hearts', type: ActivityType.Playing },
+  { name: 'you overthink everything', type: ActivityType.Watching },
+  { name: 'endless excuses', type: ActivityType.Listening },
+  { name: 'borrowed time', type: ActivityType.Playing },
+  { name: 'you complain', type: ActivityType.Listening },
+  { name: 'you lose control', type: ActivityType.Watching },
+  { name: 'bad advice', type: ActivityType.Listening },
+  { name: 'tricks on you', type: ActivityType.Playing },
+  { name: 'your bold moves', type: ActivityType.Watching },
+  { name: 'daily drama', type: ActivityType.Listening },
+  { name: 'you act tough', type: ActivityType.Watching },
+  { name: 'dumb today', type: ActivityType.Playing },
+  { name: 'your cringe texts', type: ActivityType.Watching },
+  { name: 'with fire', type: ActivityType.Playing },
 ]; //ADD ACORDING TO YOU
 
 
@@ -148,3 +181,4 @@ client.login(TOKEN);
 
 // node server start
 app.listen(PORT, () => console.log("Server Started!"));
+ 
